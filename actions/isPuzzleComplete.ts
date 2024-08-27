01ec1428bcb8f8b6f3de445460afd206500794d0
Harrison Chapman <hchapma5@gmail.com>
2024-08-27 12:48:25 +1000

    removed redundant files

--- actions/isPuzzleComplete.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-export async function isPuzzleComplete(formData: FormData) {
-  const data = Object.fromEntries(formData.entries());
-
-  // If data contains empty input return false
-  console.log(data);
-  return data;
-}
