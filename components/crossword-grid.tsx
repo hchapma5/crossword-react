8899368466da4f5a8f2e435ebf512e9188589787
Harrison Chapman <hchapma5@gmail.com>
2024-10-02 19:46:34 +1000

    fix: overflowing grid due to screenshot handler

--- components/crossword-grid.tsx
+++ components/crossword-grid.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { RefObject, useEffect, useRef } from "react";
 import { useParams } from "next/navigation";
-import ScreenCapture from "./screen-capture";
 import { checkIfThumbnailExists } from "@/db/storage";
 import { useCrossword } from "./crossword-provider";
+import { captureScreenshotAndUploadToStorage } from "@/utils/utils";
 
 export default function CrosswordGrid() {
   const {
@@ -17,30 +17,33 @@ export default function CrosswordGrid() {
     rows,
     cols,
   } = useCrossword();
-  const [needsScreenCapture, setNeedsScreenCapture] = useState(false);
 
+  const crosswordGridRef = useRef(null);
   const crosswordId = useParams().id as string;
 
   useEffect(() => {
     checkIfThumbnailExists(crosswordId).then((exists) => {
-      setNeedsScreenCapture(!exists);
+      if (exists) return;
+      captureScreenshotAndUploadToStorage(crosswordGridRef, crosswordId);
     });
   }, [crosswordId]);
 
-  const puzzleContent = (
+  return (
     <div
+      ref={crosswordGridRef}
       style={{
-        display: 'grid',
+        display: "grid",
         gridTemplateColumns: `repeat(${cols}, 1fr)`,
         gridTemplateRows: `repeat(${rows}, 1fr)`,
         aspectRatio: `${cols} / ${rows}`,
-        width: '100%',
-        gap: '1px',
+        width: "auto",
+        height: "auto",
+        gap: "1px",
       }}
-      className='max-h-fit max-w-fit'
+      className={`max-w-fit] max-h-fit`}
     >
       {Array.from({ length: rows * cols }, (_, index) => {
-        const rowIndex = Math.floor(index / cols)
+        const rowIndex = Math.floor(index / cols);
         const colIndex = index % cols;
         const position = `${rowIndex},${colIndex}`;
         const cellData = positionsMap.get(position);
@@ -54,7 +57,7 @@ export default function CrosswordGrid() {
         return (
           <div
             key={position}
-            className="relative outline outline-1 outline-black aspect-square"
+            className="relative aspect-square outline outline-1 outline-black"
           >
             {cellData.id && (
               <label className="absolute left-0 top-0 text-[1vmin] font-semibold">
@@ -72,7 +75,7 @@ export default function CrosswordGrid() {
               autoCorrect="off"
               autoComplete="off"
               onSelect={(e) => e.currentTarget.setSelectionRange(0, 1)}
-              className={`w-full h-full text-center text-[2vmin] font-semibold ${
+              className={`h-full w-full text-center text-[2vmin] font-semibold ${
                 isCurrentWord ? "bg-blue-100" : "bg-slate-100"
               }`}
               maxLength={1}
@@ -81,11 +84,5 @@ export default function CrosswordGrid() {
         );
       })}
     </div>
-  );
-
-  return needsScreenCapture ? (
-    <ScreenCapture imageId={crosswordId}>{puzzleContent}</ScreenCapture>
-  ) : (
-    puzzleContent
   );
 }
--- utils/utils.ts
+++ utils/utils.ts
@@ -1,3 +1,7 @@
+import { uploadImageToStorage } from "@/db/storage";
+import { toPng } from "html-to-image";
+import { RefObject } from "react";
+
 export async function convertBlobUrlToFile(blobUrl: string) {
   const response = await fetch(blobUrl);
   const blob = await response.blob();
@@ -7,4 +11,24 @@ export async function convertBlobUrlToFile(blobUrl: string) {
     type: mimeType,
   });
   return file;
+}
+
+export async function captureScreenshotAndUploadToStorage(
+  ref: RefObject<HTMLElement>,
+  storageId: string,
+) {
+  let image: string | undefined;
+
+  if (!ref.current) return;
+
+  try {
+    image = await toPng(ref.current);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    if (image) {
+      const file = await convertBlobUrlToFile(image);
+      await uploadImageToStorage(storageId, file);
+    }
+  }
 }
