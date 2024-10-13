1956eef4c4063191bd0ba90b61001fdb9ec16af3
Harrison Chapman <hchapma5@gmail.com>
2024-10-13 10:44:31 +1000

    switched to unstable_cache from redis

--- app/crossword/page.tsx
+++ app/crossword/page.tsx
@@ -2,12 +2,11 @@ import { redirect } from "next/navigation";
 import askGeminiForCrosswordData from "@/lib/genAI";
 import { generateCrosswordGameData } from "@/utils/crossword-utils";
 import { insertCrosswordData } from "@/db/query";
-import { currentUser } from "@clerk/nextjs/server";
+import { currentUser, User } from "@clerk/nextjs/server";
 import { redis } from "@/db/upstash";
 import { Ratelimit } from "@upstash/ratelimit";
-import CrosswordPuzzle, {
-  CrosswordPuzzleProps,
-} from "@/components/crossword-puzzle";
+import { unstable_cache } from "next/cache";
+import CrosswordPuzzle from "@/components/crossword-puzzle";
 
 // Rate limiting setup
 const rateLimit = new Ratelimit({
@@ -15,21 +14,6 @@ const rateLimit = new Ratelimit({
   limiter: Ratelimit.slidingWindow(3, "24 h"),
   analytics: true,
 });
-
-// Cache utility functions
-async function getCachedData(key: string) {
-  const cachedData = await redis.get(key);
-  if (cachedData) return cachedData;
-  return null;
-}
-
-async function setCachedData(
-  key: string,
-  data: any,
-  expirationInSeconds: number = 3600,
-) {
-  await redis.set(key, data, { ex: expirationInSeconds });
-}
 
 export default async function CrosswordPage({
   searchParams,
@@ -45,37 +29,34 @@ export default async function CrosswordPage({
   const theme = searchParams.theme;
   if (!theme) redirect("/browse");
 
-  const cacheKey = `crossword:${theme}`;
-  let crosswordData = (await getCachedData(cacheKey)) as CrosswordPuzzleProps;
+  const crosswordCache = unstable_cache(
+    async () => {
+      const { data } = await askGeminiForCrosswordData(theme);
+      const { answers, clues, positions, navigation, rows, cols } =
+        generateCrosswordGameData(data!);
 
-  if (!crosswordData) {
-    // Fetch the crossword data from Gemini
-    const { data } = await askGeminiForCrosswordData(theme);
+      const crosswordId = await insertCrosswordData({
+        theme,
+        data: data!,
+        createdBy: user.username || user.id,
+        answers: answers,
+      });
 
-    // Generate the answers to insert into the database
-    const { answers, clues, positions, navigation, rows, cols } =
-      generateCrosswordGameData(data!);
+      return {
+        crosswordId,
+        clues,
+        positions,
+        navigation,
+        rows,
+        cols,
+        theme,
+      };
+    },
+    [`crossword-data:${theme}`],
+    { revalidate: 3600 },
+  );
 
-    const crosswordId = await insertCrosswordData({
-      theme,
-      data: data!,
-      createdBy: user.username || user.id,
-      answers: answers,
-    });
-
-    crosswordData = {
-      crosswordId,
-      clues,
-      positions,
-      navigation,
-      rows,
-      cols,
-      theme,
-    };
-
-    // Cache the data
-    await setCachedData(cacheKey, crosswordData);
-  }
+  let crosswordData = await crosswordCache();
 
   return <CrosswordPuzzle {...crosswordData} />;
 }
